0153038ae3dc4fd97e430da146d4e8e462170371
Remoc-Revo <timothynguono@gmail.com>
2024-02-22 09:49:36 +0300

    Add Animated Underlines

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var scrollDistance = $(window).scrollTop();
-        var underlinePosition = $('#our-services-heading').offset().top;
-        console.log(`-${underlinePosition}--${scrollDistance}OOOOOOOOOOOOOOOOOOOOOOOOOOO1010000010000000100000`)
-        if(scrollDistance >= underlinePosition) {
+        var ourServicesPosition = $('#our-services-heading').offset().top;
+        var contactUsPosition = $('#contact-us-heading').offset().top;
+        var pricelistPosition = $('#pricelist-heading').offset().top;
+
+        //check if headings have reached the viewport
+
+        if(scrollDistance + $(window).height() >= ourServicesPosition) {
             $('#our-services-heading').addClass('active');
         }
+
+        if(scrollDistance + $(window).height() >= contactUsPosition) {
+            $('#contact-us-heading').addClass('active');
+        }
+
+        if(scrollDistance + $(window).height() >= pricelistPosition) {
+            $('#pricelist-heading').addClass('active');
+        }
     })
+
 
 })
 
